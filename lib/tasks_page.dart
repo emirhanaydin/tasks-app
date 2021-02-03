358ad1aa2d99fc07a5bbb5d39d834980fcb301f3
emirhanaydin <emirhanaydin95@gmail.com>
2021-02-03 12:57:45 +0300

    Create the basic layout of tasks page

--- lib/tasks_page.dart
+++ lib/tasks_page.dart
@@ -1,6 +1,13 @@
+import 'dart:math';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tasks_app/profile_page.dart';
+
+final _dbRef = FirebaseDatabase.instance.reference().child('tasks');
+final _auth = FirebaseAuth.instance;
 
 class TasksPage extends StatefulWidget {
   @override
@@ -8,23 +15,33 @@ class TasksPage extends StatefulWidget {
 }
 
 class _TasksPageState extends State<TasksPage> {
+  User _user;
+  DatabaseReference _dbUserRef;
+
+  _TasksPageState() {
+    _user = _auth.currentUser;
+    _dbUserRef = _dbRef.child(_user.uid);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Colors.white,
-        child: Row(
+        child: Column(
           children: [
-            Expanded(
-              child: Stack(
-                children: [
-                  Positioned(
-                    child: _profileButton(),
-                    right: 0,
-                  ),
-                ],
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                _profileButton(),
+              ],
             ),
+            Expanded(child: Text('List placeholder')),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                _addTaskButton(),
+              ],
+            )
           ],
         ),
       ),
@@ -37,6 +54,14 @@ class _TasksPageState extends State<TasksPage> {
           Navigator.of(context).push(
             MaterialPageRoute(builder: (context) => ProfilePage()),
           );
+        },
+      );
+
+  Widget _addTaskButton() => IconButton(
+        icon: Icon(Icons.add),
+        onPressed: () {
+          final ref = _dbUserRef.push();
+          ref.set('${Random().nextInt(1000)}');
         },
       );
 }
